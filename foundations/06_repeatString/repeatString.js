28d63f41aa65d4fff3dafb81a132f0009194895c
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-18 13:49:51 -0500

    Finished 06 and 07

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times >= 1) {
+        let ans = '';
+        for (let i=1; i<=times; i++) {
+            ans += str;
+        }
+        return ans;
+    } else if (times == 0) {
+        return ''
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
