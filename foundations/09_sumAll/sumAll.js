e40c0afa558178c650813995f382a30591ea8642
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-18 13:57:44 -0500

    Finished 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (!Number.isInteger(int1) || !Number.isInteger(int2) || int1 <= 0 || int2 <= 0) {
+        return 'ERROR';
+    }
+    let ans = 0;
+    for (let i=Math.min(int1, int2); i<=Math.max(int1, int2); i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
