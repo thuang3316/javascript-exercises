8acd9787a474dd82bab5529ec46300c771978cc7
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-18 14:07:07 -0500

    Finished 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  return Math.round((fahrenheitTemperature-32) * 50 / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemperature) {
+  return Math.round((celciusTemperature * 1.8 + 32)*10) / 10;
 };
 
 // Do not edit below this line
