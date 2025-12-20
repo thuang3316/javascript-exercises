efafed077a88556282f852e35cc974ffc6ee41e6
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-19 22:23:20 -0500

    Completed 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const legalLetters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const filteredString = string.toLowerCase().split('').filter(item => legalLetters.includes(item)).join('');
 
+    return filteredString === filteredString.split('').reverse().join('');
 };
 
 // Do not edit below this line
