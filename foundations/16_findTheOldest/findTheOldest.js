963cbc1f968cc7648ac30c41e1b7cfeef9315d71
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-19 22:41:39 -0500

    Finished 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson;
+    for (person of people) {
+        if (person.yearOfDeath) {
+            if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+                oldestAge = person.yearOfDeath - person.yearOfBirth;
+                oldestPerson = person;
+            }
+        } else {
+            if ((new Date()).getFullYear() - person.yearOfBirth > oldestAge) {
+                oldestAge = (new Date()).getFullYear() - person.yearOfBirth;
+                oldestPerson = person;
+            }
+        }
+    }
+    return oldestPerson;
+    
 };
 
 // Do not edit below this line
