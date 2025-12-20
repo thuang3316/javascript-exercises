cd8175f347a6da3ca0e5b0b5f74ca81a24fb8101
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-19 22:30:13 -0500

    Completed 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (typeof n === 'string') {
+        n = parseInt(n);
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    const dp = new Array(n+2).fill(0);
+    dp[0] = 1;
+    for (let i=2; i<n+2; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n+1];
 };
 
 // Do not edit below this line
